7ce3cd20361a4afee0adbd0a3ed944e207572a14
ElizavetaLevina <levina.elizaveta.2015@gmail.com>
2026-06-11 15:31:30 +0300

    Добавлены повторные попытки обработки входящих сообщений

--- ModerationService/Configurations/ConfigureServicesRabbitMQ.cs
+++ ModerationService/Configurations/ConfigureServicesRabbitMQ.cs
@@ -1,5 +1,7 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using ModerationService.BLL.Consumers;
+using Npgsql;
 
 namespace ModerationService.Configurations
 {
@@ -29,7 +31,15 @@ namespace ModerationService.Configurations
                         x.Password(configuration["RabbitMQ:Password"]);
                     });
 
-                    cfg.ConfigureEndpoints(context);
+					cfg.UseMessageRetry(x =>
+					{
+						x.Intervals(TimeSpan.FromSeconds(10), TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(10));
+						x.Handle<NpgsqlException>();
+						x.Handle<TimeoutException>();
+						x.Handle<DbUpdateException>();
+					});
+
+					cfg.ConfigureEndpoints(context);
                 });
             });
         }
